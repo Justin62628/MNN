856e178de48a71ebb76441db150cd2420e6f299d
Justin62628 <2410377391@qq.com>
2025-05-14 10:33:53 +0800

    adapt to scdet_v1_full version

--- scdet/scdet.py
+++ scdet/scdet.py
@@ -13,7 +13,7 @@ class SceneDetector:
         """
         self._detector = _SceneDetector(model_path)
 
-    def detect(self, img0: np.ndarray, img1: np.ndarray, img2: np.ndarray) -> tuple[float, float]:
+    def detect(self, img0: np.ndarray, img1: np.ndarray, img2: np.ndarray, img3: np.ndarray) -> tuple[float, float]:
         """
         执行场景检测推理。
         :param img0: HWC uint8 RGB 图像
@@ -23,7 +23,8 @@ class SceneDetector:
         assert img0.ndim == 3 and img0.dtype == np.uint8 and img0.shape[2] == 3
         assert img1.ndim == 3 and img1.dtype == np.uint8 and img1.shape[2] == 3
         assert img2.ndim == 3 and img2.dtype == np.uint8 and img2.shape[2] == 3
-        result = [1/(1+np.exp(-i)) for i in self._detector.detect(img0, img1, img2)]
+        assert img3.ndim == 3 and img3.dtype == np.uint8 and img3.shape[2] == 3
+        result = [1/(1+np.exp(-i)) for i in self._detector.detect(img0, img1, img2, img3)]
         return result
 
 
@@ -34,12 +35,22 @@ if __name__ == "__main__":
     sd = SceneDetector("D:/60-fps-Project/Projects/RIFE GUI/models/scdet/scdet.mnn")
     # 读取两张 RGB 图像
     img0 = cv2.resize(cv2.cvtColor(cv2.imread("demo/exec/test_input/input.png"), cv2.COLOR_BGR2RGB), (448, 252))
-    img1 = cv2.resize(cv2.cvtColor(cv2.imread("demo/exec/test_input/input.png"), cv2.COLOR_BGR2RGB), (448, 252))
-    # img1 = cv2.resize(cv2.cvtColor(cv2.imread("demo/exec/test_input/output.png"), cv2.COLOR_BGR2RGB), (448, 252))
-    img2 = cv2.resize(cv2.cvtColor(cv2.imread("demo/exec/test_input/output.png"), cv2.COLOR_BGR2RGB), (448, 252))
+    img1 = cv2.resize(cv2.cvtColor(cv2.imread("demo/exec/test_input/output.png"), cv2.COLOR_BGR2RGB), (448, 252))
     # 推理
 
     for i in range(1000):
         start = time.time()
-        conf0, conf1 = sd.detect(img0, img1, img2)
+        conf0, conf1 = sd.detect(img0, img0, img0, img1)
+        print(f"Confidence {i}: {conf0}, {conf1}, time: {time.time() - start}")
+
+        start = time.time()
+        conf0, conf1 = sd.detect(img0, img0, img1, img1)
+        print(f"Confidence {i}: {conf0}, {conf1}, time: {time.time() - start}")
+
+        start = time.time()
+        conf0, conf1 = sd.detect(img0, img1, img1, img1)
+        print(f"Confidence {i}: {conf0}, {conf1}, time: {time.time() - start}")
+
+        start = time.time()
+        conf0, conf1 = sd.detect(img1, img1, img1, img0)
         print(f"Confidence {i}: {conf0}, {conf1}, time: {time.time() - start}")
--- scdet/scdet_py.cpp
+++ scdet/scdet_py.cpp
@@ -22,8 +22,7 @@ public:
         ScheduleConfig config;
         config.backendConfig = &backendConfig;
         config.type = MNN_FORWARD_OPENCL;
-        config.numThread = 8;
-        config.mode = MNN_GPU_TUNING_NORMAL; // MNN_GPU_MEMORY_BUFFER, MNN_GPU_MEMORY_IMAGE?
+        config.mode = MNN_GPU_TUNING_WIDE; // MNN_GPU_MEMORY_BUFFER, MNN_GPU_MEMORY_IMAGE?
 
         session = net->createSession(config);
 
@@ -31,6 +30,7 @@ public:
         input0 = net->getSessionInput(session, "x0");
         input1 = net->getSessionInput(session, "x1");
         input2 = net->getSessionInput(session, "x2");
+        input3 = net->getSessionInput(session, "x3");
         auto shape = input0->shape();
         size_w = shape[3]; size_h = shape[2];
 
@@ -47,19 +47,22 @@ public:
         temp_host0.reset(new Tensor(input0, Tensor::CAFFE));
         temp_host1.reset(new Tensor(input1, Tensor::CAFFE));
         temp_host2.reset(new Tensor(input2, Tensor::CAFFE));
+        temp_host3.reset(new Tensor(input3, Tensor::CAFFE));
     }
 
     std::pair<float, float> detect(
         py::array_t<uint8_t, py::array::c_style | py::array::forcecast> img0,
         py::array_t<uint8_t, py::array::c_style | py::array::forcecast> img1,
-        py::array_t<uint8_t, py::array::c_style | py::array::forcecast> img2)
+        py::array_t<uint8_t, py::array::c_style | py::array::forcecast> img2,
+        py::array_t<uint8_t, py::array::c_style | py::array::forcecast> img3)
     {
         // —— 获取 NumPy 缓冲区 —— 
-        auto b0 = img0.request(), b1 = img1.request(), b2 = img2.request();
+        auto b0 = img0.request(), b1 = img1.request(), b2 = img2.request(), b3 = img3.request();
         int H = b0.shape[0], W = b0.shape[1];
         auto ptr0 = static_cast<uint8_t*>(b0.ptr);
         auto ptr1 = static_cast<uint8_t*>(b1.ptr);
         auto ptr2 = static_cast<uint8_t*>(b2.ptr);
+        auto ptr3 = static_cast<uint8_t*>(b3.ptr);
 
         // —— 尺寸变更时，更新缩放矩阵 —— 
         if (cached_w != W || cached_h != H) {
@@ -79,10 +82,13 @@ public:
                           temp_host1->host<uint8_t>(), size_w, size_h, 3, 0, temp_host1->getType());
         pretreat->convert(ptr2, W, H, 0,
                           temp_host2->host<uint8_t>(), size_w, size_h, 3, 0, temp_host2->getType());
+        pretreat->convert(ptr3, W, H, 0,
+                          temp_host3->host<uint8_t>(), size_w, size_h, 3, 0, temp_host3->getType());
 
         input0->copyFromHostTensor(temp_host0.get());
         input1->copyFromHostTensor(temp_host1.get());
         input2->copyFromHostTensor(temp_host2.get());
+        input3->copyFromHostTensor(temp_host3.get());
         {
             py::gil_scoped_release release;
             net->runSession(session);
@@ -94,17 +100,17 @@ public:
         output->copyToHostTensor(&result);
         float *out_data = result.host<float>();
         int stride = result.stride(1);
-        return { out_data[0], out_data[0] };
+        return { out_data[0], out_data[1] };
     }
 
 private:
     std::shared_ptr<Interpreter> net;
     Session *session;
-    Tensor *input0, *input1, *input2;
+    Tensor *input0, *input1, *input2, *input3;
     int size_w, size_h, cached_w, cached_h;
     std::unique_ptr<Matrix> trans;
     std::shared_ptr<ImageProcess> pretreat;
-    std::unique_ptr<Tensor> temp_host0, temp_host1, temp_host2;
+    std::unique_ptr<Tensor> temp_host0, temp_host1, temp_host2, temp_host3;
 };
 
 PYBIND11_MODULE(scdet_mnn, m) {
